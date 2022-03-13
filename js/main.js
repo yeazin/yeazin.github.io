9caa276b2cd136583b548931837f890c8890111a
yeazin <naz.yeasin@gmail.com>
2022-03-13 14:49:30 +0600

    added code base

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+let url = "https://api.github.com/users/yeazin"
+
+function APIFetch(){
+  fetch(url)
+    .then((res)=>{
+      return res.json()
+    })
+    .then((data)=>{
+      console.log(data);
+      document.getElementById('name').innerHTML = data.name;
+      document.getElementById('img').src = data.avatar_url;
+      document.getElementById("username").innerHTML = '@'+data.login;
+      document.getElementById("username").href = data.html_url;
+      document.getElementById("bio").innerHTML = data.bio;
+
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+};
+
+APIFetch()
